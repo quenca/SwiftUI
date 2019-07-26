cdd0f9ee3a9a09b6b341e7e22f876df5ec302b52
Gustavo Pereira Teixeira Quenca <gustavo.quenca@mc1.com.br>
2019-07-26 13:42:55 -0300

    Initial Commit

---
+++ MovieSwiftUI/MovieSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MovieSwiftUI
+//
+//  Created by Gustavo Pereira Teixeira Quenca on 26/07/19.
+//  Copyright © 2019 Gustavo Pereira Teixeira Quenca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ MovieSwiftUI/MovieSwiftUITests/MovieSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieSwiftUITests.swift
+//  MovieSwiftUITests
+//
+//  Created by Gustavo Pereira Teixeira Quenca on 26/07/19.
+//  Copyright © 2019 Gustavo Pereira Teixeira Quenca. All rights reserved.
+//
+
+import XCTest
+@testable import MovieSwiftUI
+
+class MovieSwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
